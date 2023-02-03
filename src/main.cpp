b660151c51f7950e74812edc1d6b62c944e76800
Lucio Anderlini <lucio.anderlini@fi.infn.it>
2023-02-03 14:25:53 +0100

    updated pipeline

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,9 @@
 #include "SQLamarr/MCParticleSelector.h"
 #include "SQLamarr/PVReconstruction.h"
 #include "SQLamarr/Plugin.h"
+#include "SQLamarr/GenerativePlugin.h"
+#include "SQLamarr/GlobalPRNG.h"
+#include "SQLamarr/TemporaryTable.h"
 #include <memory>
 #include <glob.h>
 
@@ -37,6 +40,7 @@ int main(int argc, char* argv[])
 //
   // Create the in-memory database
   SQLamarr::SQLite3DB db = SQLamarr::make_database(argc > 1 ? argv[1] : ":memory:" );
+  SQLamarr::GlobalPRNG::get_or_create(db.get(), 123);
 
 
   // Define a data loader to take data from HepMC2-Ascii format
@@ -47,7 +51,7 @@ int main(int argc, char* argv[])
   size_t runNumber = 456;
 
   for (std::string& file_path: file_paths)
-    if (evtNumber < 1) 
+    if (evtNumber < 10) 
       loader.load(file_path, runNumber, evtNumber++);
 
   // Runs the PVFinder algorithm
@@ -95,7 +99,144 @@ int main(int argc, char* argv[])
       );
 
   acceptance_model.execute();
-  
+
+
+  SQLamarr::Plugin efficiency_model (db,
+        "../temporary_data/models/lhcb.trk.2016MU.so",
+        "efficiency", 
+        R"(
+        SELECT 
+          mcparticle_id,
+          ov.x AS mc_x, 
+          ov.y AS mc_y, 
+          ov.z AS mc_z,
+          log10(norm2(p.px, p.py, p.pz)) AS mc_log10_p,
+          p.px/p.pz AS mc_tx, 
+          p.py/p.pz AS mc_ty,
+          pseudorapidity(p.px, p.py, p.pz) AS mc_eta,
+          azimuthal(p.px, p.py, p.pz) AS mc_phi,
+          abs(p.pid) == 11 AS mc_is_e,
+          abs(p.pid) == 13 AS mc_is_mu,
+          (
+            abs(p.pid) == 211 OR abs(p.pid) == 321 OR abs(p.pid) == 2212  
+          ) AS mc_is_h,
+          propagation_charge(p.pid) AS mc_charge
+        FROM MCParticles AS p
+        INNER JOIN MCVertices AS ov ON p.production_vertex = ov.mcvertex_id
+        WHERE 
+          p.pz > 1.
+          AND
+          propagation_charge(p.pid) <> 0.
+        )",
+        "tmp_efficiency_out", 
+        {"not_recoed", "long", "upstream", "downstream"}, 
+        {"mcparticle_id"}
+      );
+
+  efficiency_model.execute();
+
+  SQLamarr::TemporaryTable assign_track_cat (db,
+      "tmp_particles_recoed_as", {"mcparticle_id", "track_type"},
+      R"( SELECT 
+            eff.mcparticle_id,
+            random_category(not_recoed, 0, 0, long, upstream) AS track_type
+          FROM tmp_efficiency_out AS eff
+          INNER JOIN tmp_acceptance_out AS acc
+            ON eff.mcparticle_id = acc.mcparticle_id
+          WHERE random_category(1 - acc.acceptance, acc.acceptance) == 1
+      )");
+
+  assign_track_cat.execute();
+
+  SQLamarr::GenerativePlugin resolution_model (db,
+        "../temporary_data/models/lhcb.trk.2016MU.so",
+        "resolution", 
+        R"(
+        SELECT 
+          p.mcparticle_id,
+          ov.x AS mc_x, -- This is wrong! Should use ClosestToBeam instead!!!
+          ov.y AS mc_y, -- WRONG!
+          ov.z AS mc_z, -- WRONG!
+          p.px/p.pz AS mc_tx, 
+          p.py/p.pz AS mc_ty,
+          log10(norm2(p.px, p.py, p.pz)) AS mc_log10_p,
+          abs(p.pid) == 11 AS mc_is_e,
+          abs(p.pid) == 13 AS mc_is_mu,
+          (abs(p.pid) = 211 OR abs(p.pid) = 321 OR abs(p.pid) = 2212) AS is_h,
+          (recguess.track_type == 3) AS is_long, 
+          (recguess.track_type == 4) AS is_upstream, 
+          (recguess.track_type == 5) AS is_downstream 
+        FROM MCParticles AS p
+        INNER JOIN MCVertices AS ov ON p.production_vertex = ov.mcvertex_id
+        INNER JOIN tmp_particles_recoed_as AS recguess 
+          ON p.mcparticle_id = recguess.mcparticle_id
+        WHERE 
+          recguess.track_type BETWEEN 3 AND 5;
+        )",
+        "tmp_resolution_out", 
+        {"dx", "dy", "dz", "dtx", "dty", "dp", 
+          "chi2PerDoF", "nDoF_f", "ghostProb"}, 
+        128,
+        {"mcparticle_id"}
+      );
+
+  resolution_model.execute();
+
+
+  SQLamarr::GenerativePlugin covariance_model (db,
+        "../temporary_data/models/lhcb.trk.2016MU.so",
+        "covariance", 
+        R"(
+        SELECT 
+          p.mcparticle_id,
+          ov.x AS mc_x, -- This is wrong! Should use ClosestToBeam instead!!!
+          ov.y AS mc_y, -- WRONG!
+          ov.z AS mc_z, -- WRONG!
+          p.px/p.pz AS mc_tx, 
+          p.py/p.pz AS mc_ty,
+          log10(norm2(p.px, p.py, p.pz)) AS mc_log10_p,
+          tmpres.chi2PerDoF AS chi2PerDoF,
+          tmpres.nDoF_f AS nDoF_f,
+          tmpres.ghostProb AS ghostProb,
+          abs(p.pid) == 11 AS mc_is_e,
+          abs(p.pid) == 13 AS mc_is_mu,
+          (abs(p.pid) = 211 OR abs(p.pid) = 321 OR abs(p.pid) = 2212) AS is_h,
+          (recguess.track_type == 3) AS is_long, 
+          (recguess.track_type == 4) AS is_upstream, 
+          (recguess.track_type == 5) AS is_downstream 
+        FROM MCParticles AS p
+        INNER JOIN MCVertices AS ov ON p.production_vertex = ov.mcvertex_id
+        INNER JOIN tmp_particles_recoed_as AS recguess 
+          ON p.mcparticle_id = recguess.mcparticle_id
+        INNER JOIN tmp_resolution_out AS tmpres 
+          ON p.mcparticle_id = tmpres.mcparticle_id
+        WHERE 
+          recguess.track_type BETWEEN 3 AND 5;
+        )",
+        "tmp_covariance_out", 
+        {
+          "log_cov_ClosestToBeam_0_0",
+          "log_cov_ClosestToBeam_1_1",
+          "log_cov_ClosestToBeam_2_2",
+          "log_cov_ClosestToBeam_3_3",
+          "log_cov_ClosestToBeam_4_4",
+          "corr_ClosestToBeam_0_1",
+          "corr_ClosestToBeam_0_2",
+          "corr_ClosestToBeam_1_2",
+          "corr_ClosestToBeam_0_3",
+          "corr_ClosestToBeam_1_3",
+          "corr_ClosestToBeam_2_3",
+          "corr_ClosestToBeam_0_4",
+          "corr_ClosestToBeam_1_4",
+          "corr_ClosestToBeam_2_4",
+          "corr_ClosestToBeam_3_4"
+        }, 
+        128,
+        {"mcparticle_id"}
+      );
+
+  covariance_model.execute();
+
 
   std::cout << SQLamarr::dump_table(db, "SELECT COUNT(*) as n_files FROM DataSources") << std::endl;
   std::cout << SQLamarr::dump_table(db, "SELECT * FROM DataSources LIMIT 10") << std::endl;
@@ -121,9 +262,25 @@ int main(int argc, char* argv[])
     )");
 
   std::cout << SQLamarr::dump_table(db, "SELECT * FROM Vertices LIMIT 10") << std::endl;
+
   std::cout << SQLamarr::dump_table(db, R"(
-    SELECT *, random_uniform() < acceptance AS in_acceptance
-     FROM tmp_acceptance_out LIMIT 10)") << std::endl;
+    SELECT acc.acceptance AS acc, eff.*,
+      not_recoed + long + upstream + downstream AS norm,
+      red.track_type
+      FROM tmp_efficiency_out as eff
+      INNER JOIN tmp_acceptance_out as acc 
+        ON eff.mcparticle_id = acc.mcparticle_id
+      LEFT JOIN tmp_particles_recoed_as AS red
+        ON eff.mcparticle_id = red.mcparticle_id
+      LIMIT 10)") << std::endl;
+
+  std::cout << SQLamarr::dump_table(db, R"(
+      SELECT * FROM tmp_resolution_out
+    )");
+
+  std::cout << SQLamarr::dump_table(db, R"(
+      SELECT * FROM tmp_covariance_out
+    )");
      
 
   return 0;
