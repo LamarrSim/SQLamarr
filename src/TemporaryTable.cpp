a0f8f5eaeb85b0faa8151e95bd565769fa742ce2
Lucio Anderlini <Lucio.Anderlini@cern.ch>
2023-02-15 15:11:23 +0100

    Added copyright statements

--- src/TemporaryTable.cpp
+++ src/TemporaryTable.cpp
@@ -1,3 +1,13 @@
+// (c) Copyright 2022 CERN for the benefit of the LHCb Collaboration.
+//
+// This software is distributed under the terms of the GNU General Public
+// Licence version 3 (GPL Version 3), copied verbatim in the file "LICENCE".
+//
+// In applying this licence, CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+
 // STL
 #include <sstream>
 
